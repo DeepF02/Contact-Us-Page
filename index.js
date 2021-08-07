0d8e9902207b68dd79663faa21aada713f75d374
Deep Fumtiya <deepfumtiya210@gmail.com>
2021-08-08 00:56:51 +0530

    added netlify connection

--- index.js
+++ index.js
@@ -31,24 +31,6 @@ app.get('/contact/submit', (req, res) => {
 });
 
 app.post('/contact', async (req, res) => {
-    // const form = document.querySelector('#contactForm');
-    // form.addEventListener('submit', async event => {
-        // event.preventDefault();
-        // const serviceID = 'default_service';
-        // const templateID = 'contact_form';
-        // let userDetails = {
-        //     to_name: document.querySelector('#username').value,
-        //     user_email: document.querySelector('#userEmail').value,
-        // };
-        // await emailjs.send(serviceID, templateID, userDetails)
-        //     .then(res => {
-        //         console.log("success", res.status);
-        //         alert('Sent!');
-        //     })
-            // }, err => {
-            //     alert(JSON.stringify(err));
-            // });
-    // });
     const contactInfo = new contactModel(req.body.contact);
     await contactInfo.save();
     res.redirect(`/contact/submit`);
